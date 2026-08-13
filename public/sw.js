56125e815bff8057cdd63417695d0ad56ebffc5b
arambarzani07 <arambarzani070@gmail.com>
2026-08-13 09:23:13 +0300

    Bind each market account to one market

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "zhirox-pos-shell-v25";
+const CACHE_NAME = "zhirox-pos-shell-v26";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/favicon.svg"];
 
 self.addEventListener("install", (event) => {
