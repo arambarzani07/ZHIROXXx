7d25b208747cbe5c997dfd1809a24d87f7967688
arambarzani07 <arambarzani070@gmail.com>
2026-08-13 08:49:21 +0300

    Add secure multi-market switching

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "zhirox-pos-shell-v22";
+const CACHE_NAME = "zhirox-pos-shell-v23";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/favicon.svg"];
 
 self.addEventListener("install", (event) => {
